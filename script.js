27df9d7402db8d40db8248ea421a453bc933ba06
MrLuckyCat <joewilliamdd31@gmail.com>
2022-12-05 21:54:12 +0100

    Make the form closable and openable

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-let imageInput = document.getElementById("book-image");
-let btn = document.getElementById("add-image-btn");
+let libray = document.querySelector(".library"),
+    addBookBtn = document.querySelector(".add-book"),
+    form = document.querySelector("form"),
+    imageInput = document.getElementById("book-image"),
+    addImageBtn = document.getElementById("add-image-btn"),
+    closeBtn = document.querySelector(".close-form-btn");
 
-btn.onclick = () => imageInput.click();
+function closeForm() {
+    closeBtn.style.display = "none";
+    form.style.display = "none";
+    document
+        .querySelector(":root")
+        .style.setProperty("--black-transparent-background", "initial");
+    libray.style.zIndex = "initial";
+}
+
+function openForm() {
+    closeBtn.style.display = "initial";
+    form.style.display = "initial";
+    document
+        .querySelector(":root")
+        .style.setProperty(
+            "--black-transparent-background",
+            "linear-gradient(#0002, #0002)"
+        );
+    libray.style.zIndex = "-1";
+}
+
+addBookBtn.onclick = openForm;
+addImageBtn.onclick = () => imageInput.click();
+closeBtn.onclick = closeForm;
