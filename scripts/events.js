4ec85a7aa6291cfc00e1b13d9f685dd97412d043
The Wonderful Cat <joewilliamdd31@gmail.com>
2023-04-14 23:01:09 +0100

    Delete the form submit buttons events and use just one event

--- scripts/events.js
+++ scripts/events.js
@@ -6,9 +6,7 @@ trashBtn.onmouseleave = changeTrashColor;
 editBtn.onmouseover = changePenColor;
 editBtn.onmouseleave = changePenColor;
 editBtn.onclick = openForm;
-editBookBtn.onclick = editBookInformation;
 window.onload = retrieveLibrary;
 
 addImageBtn.onclick = () => imageInput.click();
-submitBtn.onclick = addBookToLibrary;
 closeBtn.onclick = closeForm;
