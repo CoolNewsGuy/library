c7a3ea0a7b3997b99e1a68b0b7aa8a80a01d4ac5
The Wonderful Cat <joewilliamdd31@gmail.com>
2023-04-15 11:11:21 +0100

    Add the error messages that will be used for invalid inputs

---
+++ scripts/CustomValidator.js
@@ -0,0 +1,10 @@
+class CustomValidator {
+    constructor() {
+        this.errorMessages = {
+            emptyInputError: "Please fill the input",
+            authorNameError: "The name must only contains letters (A-Z)",
+            bookImageError: "Upload an image",
+            bookPagesError: "Enter the number of the book's pages",
+        };
+    }
+}
